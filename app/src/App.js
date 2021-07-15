1706ba5c6c1777f2ca1fa55c05a743e5d35f6080
sbalayan1 <balayans2014@yahoo.com>
2021-07-15 10:59:16 -0700

    working on cat card

--- app/src/App.js
+++ app/src/App.js
@@ -13,7 +13,7 @@ import { Route, Switch, useHistory } from 'react-router-dom'
 
   let App = () => {
       // set login validated to true if you want to work on the rest of the app
-    const [loginValidated, setLoginValidated] = useState(false)
+    const [loginValidated, setLoginValidated] = useState(true)
     const [visitorData, setVisitorData] = useState([])
     const [animalData, setAnimalData] = useState([])
     const [shelterData, setShelterData] = useState([])
