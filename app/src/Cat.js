5ceb511e343d9c1c98b4e06da0a8591efd2dba8c
sbalayan1 <balayans2014@yahoo.com>
2021-07-15 13:48:26 -0700

    pushing again

--- app/src/Cat.js
+++ app/src/Cat.js
@@ -21,14 +21,14 @@ export default function Cat(animalData) {
             <div className="animal-container">
                 
                 <div className="animal-card">
-                    <img className="animal-image" src='http://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Neighbours_Siamese.jpg/640px-Neighbours_Siamese.jpg' alt="picture of cat"/>           
+                    <img className="animal-image" src='http://petnaturals.com/blog/wp-content/uploads/2013/08/cat-in-space.jpg' alt="picture of cat"/>           
                     <h4>Name: {cat1.name}</h4>
                     <h6>Age: {cat1.age}</h6>
                     <h6>Breed: {cat1.breed}</h6>
                     <button className="adopt-button">Adopt</button>
                 </div> 
                 <div className="animal-card">
-                    <img className="animal-image" src='https://i.natgeofe.com/n/f0dccaca-174b-48a5-b944-9bcddf913645/01-cat-questions-nationalgeographic_1228126.jpg' alt="picture of cat"/>           
+                    <img className="animal-image" src='https://i1.wp.com/katzenworld.co.uk/wp-content/uploads/2019/06/funny-cat.jpeg?fit=1920%2C1920&ssl=1' alt="picture of cat"/>           
                     <h4>Name: {cat2.name}</h4>
                     <h6>Age: {cat2.age}</h6>
                     <h6>Breed: {cat2.breed}</h6>
@@ -56,7 +56,7 @@ export default function Cat(animalData) {
                     <button className="adopt-button">Adopt</button>
                 </div> 
                 <div className="animal-card">
-                    <img className="animal-image" src='https://cdn.vox-cdn.com/thumbor/p2x3cL6eHvyco1RRdIarPHjsZGg=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19539772/cats4.jpg' alt="picture of cat"/>           
+                    <img className="animal-image" src='https://static.boredpanda.com/blog/wp-content/uploads/2014/02/funny-wet-cats-coverimage.jpg' alt="picture of cat"/>           
                     <h4>Name: {cat6.name}</h4>
                     <h6>Age: {cat6.age}</h6>
                     <h6>Breed: {cat6.breed}</h6>
