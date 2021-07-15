dafdb96f02cc83cdbcac4658ae7bbcef91779516
Justin <jdanielmcarthur@msn.com>
2021-07-15 07:58:17 -0700

    more styling on home page

--- app/src/Home.js
+++ app/src/Home.js
@@ -7,8 +7,10 @@ let Home = (visitorData) => {
 
     return (
         <div>
-            <div className="homescreen_headers">
+            <div className="homescreen_header">
                 <h1>WestCoast WyldLyfe</h1>
+            </div>
+            <div className="homescreen_about">
                 <h4>WestCoast WyldLyfe is an application that helps YOU find your new animal companion. By finding the nearest animal shelters to you, we hope to make the process of finding and giving these lovely animals a new home as easy as possible. Established in the summer of 2021, WestCoast Wyldlyfe has already helped dozens of animals find a new family.</h4>
             </div>
             <div className="homescreen_cards">
